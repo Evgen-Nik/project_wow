664b1120f4331c506e0c09442dde189af3ee1501
Evgen_Nik <nesergeevich93.com>
2024-01-09 13:58:34 +0200

    1 commit

--- src/js/modules/sliSlider.js
+++ src/js/modules/sliSlider.js
@@ -7,35 +7,21 @@ function sliSlider(container) {
         infinite: false,
         speed: 300,
         slidesToShow: 3,
-        slidesToScroll: 1
-        // responsive: [
-        //   {
-        //     breakpoint: 1024,
-        //     settings: {
-        //       slidesToShow: 3,
-        //       slidesToScroll: 3,
-        //       infinite: true,
-        //       dots: true
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 600,
-        //     settings: {
-        //       slidesToShow: 2,
-        //       slidesToScroll: 2
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 480,
-        //     settings: {
-        //       slidesToShow: 1,
-        //       slidesToScroll: 1
-        //     }
-        //   }
-        //   // You can unslick at a given breakpoint now by adding:
-        //   // settings: "unslick"
-        //   // instead of a settings object
-        // ]
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 993,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 577,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
     });
 
     const newSlide = card();
